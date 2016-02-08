b823dbecf0f96320ed6bc2c895ef9a902aa85f94
Michaela Ivanova <michaela.av.ivanova@gmail.com>
2016-02-08 11:24:06 +0200

    Added Like class
    Test branching

---
+++ Source/Data/ReaLocate.Models/Like.cs
@@ -0,0 +1,13 @@
+﻿namespace ReaLocate.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Like
+    {
+        public int Value { get; set; }
+    }
+}
