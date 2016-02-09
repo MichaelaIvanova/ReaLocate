b601c4b26904bbb58709b67473f4a05d6b0eea05
Michaela Ivanova <michaela.av.ivanova@gmail.com>
2016-02-09 13:31:43 +0200

    Added basic geo guide
    Multiple markers & stand alone marker

---
+++ Geolocation/GoogleMaps/GoogleMapsMVC/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Geocoding.Google;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace GoogleMapsMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            GoogleGeocoder geocoder = new GoogleGeocoder();
+            List<GoogleAddress> addresses = geocoder.Geocode("Metodi Andonov 107, 1797 Sofia, Bulgaria").ToList();
+
+            var coordinates = addresses[0].Coordinates;
+            var address = @addresses[0].FormattedAddress;
+
+            var c = new Coordinate() { Address = address, Lat= coordinates.Latitude, Long = coordinates.Longitude };
+
+            return View(c);
+        }
+
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+    }
+
+    public class Coordinate
+    {
+        public double Long { get; set; }
+
+        public double Lat { get; set; }
+
+        public string Address { get; set; }
+    }
+}
