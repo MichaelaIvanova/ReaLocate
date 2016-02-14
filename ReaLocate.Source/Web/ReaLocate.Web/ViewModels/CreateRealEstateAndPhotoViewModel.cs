333f8e40ddd1cea8010abba5b8d6bb82c4e386b4
Michaela Ivanova <michaela.av.ivanova@gmail.com>
2016-02-14 18:22:26 +0200

    Client side file upload validation added

---
+++ ReaLocate.Source/Web/ReaLocate.Web/ViewModels/CreateRealEstateAndPhotoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReaLocate.Web.ViewModels
+{
+    public class CreateRealEstateAndPhotoViewModel
+    {
+        public CreateRealEstateViewModel RealEstate { get; set; }
+
+        public IList<CreatePhotoViewModel> RealEstatePhotos { get; set; }
+    }
+}
