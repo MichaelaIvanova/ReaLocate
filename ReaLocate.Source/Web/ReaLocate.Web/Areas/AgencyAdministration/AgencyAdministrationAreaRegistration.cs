a57d1c5f926fc855289de5ab7105553fe01733ae
Michaela Ivanova <michaela.av.ivanova@gmail.com>
2016-02-22 14:06:57 +0200

    Added partial menu view

---
+++ ReaLocate.Source/Web/ReaLocate.Web/Areas/AgencyAdministration/AgencyAdministrationAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ReaLocate.Web.Areas.AgencyAdministration
+{
+    public class AgencyAdministrationAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "AgencyAdministration";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "AgencyAdministration_default",
+                "AgencyAdministration/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ReaLocate.Source/Web/ReaLocate.Web/Areas/AgencyAdministration/Controllers/MyAgencyAdminController.cs
@@ -0,0 +1,22 @@
+﻿namespace ReaLocate.Web.Areas.AgencyAdministration.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using Web.Controllers;
+
+    [Authorize(Roles = "AgencyOwner")]
+    [ValidateInput(false)]
+    public class MyAgencyAdminController : BaseController
+    {
+        // GET: AgencyAdministration/MyAgencyAdmin/Menu
+        [ChildActionOnly]
+        public ActionResult Menu()
+        {
+            IEnumerable<string> items = new List<string>() { "RealEstateOffers", "MyAgencyInvoices" };
+            return this.PartialView("_AdminMenu", items);
+        }
+    }
+}
