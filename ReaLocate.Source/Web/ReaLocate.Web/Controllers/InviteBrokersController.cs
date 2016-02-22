d248e9ccf08452d25c9273e2c2299e4f1b96696e
Michaela Ivanova <michaela.av.ivanova@gmail.com>
2016-02-22 17:02:07 +0200

    Added user seach by username, allowed only for agencies to hire brokers

---
+++ ReaLocate.Source/Web/ReaLocate.Web/Controllers/InviteBrokersController.cs
@@ -0,0 +1,46 @@
+﻿namespace ReaLocate.Web.Controllers
+{
+    using Services.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using ViewModels;
+
+    [Authorize(Roles = "AgencyOwner")]
+    public class InviteBrokersController : Controller
+    {
+        private readonly IUsersService usersService;
+
+        public InviteBrokersController(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+
+        [HttpGet]
+        public ActionResult InviteBrokers()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult InviteBrokers(string id, BrokerInputViewModel broker)
+        {
+
+            return this.RedirectToAction("AgencyDetails", "Agencies", new { id = id });
+        }
+
+        public ActionResult GetAllUsersUserNames()
+        {
+            //TODO: Cache it
+            var users = this.usersService.GetAll()
+                .Where(u => u.MyOwnAgencyId == null && u.AgencyWorkForId == null)
+                .Select(u => u.UserName)
+                .ToList();
+
+            return this.Json(new[] { users });
+        }
+    }
+}
---
+++ ReaLocate.Source/Web/ReaLocate.Web/ViewModels/BrokerInputViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ReaLocate.Data.Models;
+using ReaLocate.Web.Infrastructure.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReaLocate.Web.ViewModels
+{
+    public class BrokerInputViewModel : IMapFrom<User>, IMapTo<User>
+    {
+        public string UserName { get; set; }
+    }
+}
